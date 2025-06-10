66fdc846c0d87dff5795d833a068f8c90d20314d
abstrxcted <cda0420@gmail.com>
2025-06-10 04:14:01 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+To make this work you have to put in a .env file into the main folder with your discord bot token, and your groq api key
+I also used a venv to make this work hence the requirements.txt so you can prepare it on your own
+- Chris
---
+++ main.py
@@ -0,0 +1,59 @@
+from groq import Groq
+import os
+from dotenv import load_dotenv
+import discord
+from discord.ext import commands
+
+#Loading keys/tokens from .env
+load_dotenv()
+
+#Load context file
+with open("context.txt", "r", encoding="utf-8") as f:
+    context = f.read()
+#Discord Bot Setup
+intents = discord.Intents.default()
+intents.message_content = True
+intents.messages = True
+intents.guilds = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+#Set Groq API Key
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY"),
+)
+
+#On bot log in
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return  # Ignore the bot's own messages
+    await bot.process_commands(message)
+
+#Main query function
+@bot.command()
+# * means everything after the command string is one text
+# message: str = "" provides a fallback if the message has no extra text
+async def yochillguy(ctx, *, message: str = "hi"):
+    chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": f"""message: {message} {context}""",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+    print(f"{ctx.author} said \"{message}\", and Chillguybot replied with: \"{chat_completion.choices[0].message.content}\"")
+
+
+    await ctx.send(chat_completion.choices[0].message.content)
+
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
+
